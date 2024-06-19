bb3a6b4834643037b4460c8cde89b3494ebfc41c
oussama El mazali <132847557+BANKLORD@users.noreply.github.com>
2024-06-19 12:09:37 +0100

    fix

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,34 +1,33 @@
 #!/usr/bin/env python3
-""" expiring web cache module """
+"""Implementing an expiring web cache and tracker """
 
-import redis
 import requests
-from typing import Callable
+import redis
 from functools import wraps
+from typing import Callable
+redis_client = redis.Redis()
 
-redis = redis.Redis()
 
-
-def wrap_requests(fn: Callable) -> Callable:
-    """ Decorator wrapper """
-
-    @wraps(fn)
-    def wrapper(url):
-        """ Wrapper for decorator guy """
-        redis.incr(f"count:{url}")
-        cached_response = redis.get(f"cached:{url}")
-        if cached_response:
-            return cached_response.decode('utf-8')
-        result = fn(url)
-        redis.setex(f"cached:{url}", 10, result)
-        return result
-
+def url_count(method: Callable) -> Callable:
+    """setting the decorator"""
+    @wraps(method)
+    def wrapper(*args, **kwargs):
+        url = args[0]
+        redis_client.incr(f"count:{url}")
+        cached = redis_client.get(f'{url}')
+        if cached:
+            return cached.decode('utf-8')
+        redis_client.setex(f'{url}, 10, {method(url)}')
+        return method(*args, **kwargs)
     return wrapper
 
 
-@wrap_requests
+@url_count
 def get_page(url: str) -> str:
-    """get page self descriptive
-    """
+    """get the page """
     response = requests.get(url)
     return response.text
+
+
+if __name__ == "__main__":
+    get_page('http://slowly.robertomurray.co.uk')
